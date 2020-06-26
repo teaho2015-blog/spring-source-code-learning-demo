897b8933ae08e79e21c170f6914f60ea587542da
teaho2015 <teaho2015@gmail.com>
2020-06-27 00:06:37 +0800

    upd

---
+++ spring-boot-mvc-shutdown/src/main/java/net/teaho/demo/spring/web/server/shutdown/bean/SimpleBean.java
@@ -0,0 +1,33 @@
+package net.teaho.demo.spring.web.server.shutdown.bean;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PreDestroy;
+
+/**
+ * @author hetl@akulaku.com
+ * @date 2020-06
+ * @since 1.0.0
+ */
+@Slf4j
+public class SimpleBean implements DisposableBean {
+    @Override
+    public void destroy() throws Exception {
+        log.info("DisposableBean is destroying!");
+    }
+
+
+    public void selfDestroy() {
+
+        log.info("On my way to destroy!");
+    }
+
+    @PreDestroy
+    public void preDestroy() {
+
+        log.info("@PreDestroy!");
+
+    }
+}
