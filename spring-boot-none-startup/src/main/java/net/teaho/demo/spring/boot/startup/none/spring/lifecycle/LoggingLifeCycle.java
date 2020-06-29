f90206bbbe094bf0745866f21f67868d3ddf3997
teaho2015 <teaho2015@gmail.com>
2020-06-29 22:58:59 +0800

    upd

---
+++ spring-boot-none-startup/src/main/java/net/teaho/demo/spring/boot/startup/none/spring/lifecycle/LoggingLifeCycle.java
@@ -0,0 +1,40 @@
+package net.teaho.demo.spring.boot.startup.none.spring.lifecycle;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.SmartLifecycle;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+/**
+ * @author hetl@akulaku.com
+ * @date 2020-06
+ * @since 1.0.0
+ */
+@Slf4j
+@Component
+public class LoggingLifeCycle implements SmartLifecycle {
+
+
+    private final AtomicBoolean running = new AtomicBoolean(false);
+
+    @Override
+    public void start() {
+        if (running.compareAndSet(false, true)) {
+            log.info("In Life cycle bean start().");
+        }
+    }
+
+    @Override
+    public void stop() {
+        if (running.compareAndSet(true, false)) {
+            log.info("In Life cycle bean stop().");
+        }
+
+    }
+
+    @Override
+    public boolean isRunning() {
+        return running.get();
+    }
+}
