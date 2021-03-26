1240470f719b03ef690eff37c7f3390569c46a0f
teaho2015 <teaho2015@gmail.com>
2021-03-26 23:38:58 +0800

    [init]spring cloud web

--- demo-web-api/src/main/java/net/teaho/demo/spring/web/api/config/ClientAutoConfiguration.java
+++
@@ -1,9 +0,0 @@
-package net.teaho.demo.spring.web.api.config;
-
-import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@EnableFeignClients("net.teaho.demo.spring.web.api")
-public class ClientAutoConfiguration {
-}
---
+++ spring-cloud-web-demo/spring-cloud-web-demo-service/src/main/java/net/teaho/demo/spring/cloud/web/server/spring/InstantiationTracingBeanPostProcessor.java
@@ -0,0 +1,25 @@
+package net.teaho.demo.spring.cloud.web.server.spring;
+
+import org.springframework.beans.factory.config.BeanPostProcessor;
+import org.springframework.stereotype.Component;
+
+/**
+ * @author teaho2015
+ * @date 2020-05
+ * @since 1.0.0
+ */
+@Component
+public class InstantiationTracingBeanPostProcessor implements BeanPostProcessor {
+
+    @Override
+    public Object postProcessBeforeInitialization(Object bean, String beanName) {
+        // we could potentially return any object reference here...
+        return bean;
+    }
+
+    @Override
+    public Object postProcessAfterInitialization(Object bean, String beanName) {
+        System.out.println("Bean '" + beanName + "' created : " + bean.toString());
+        return bean;
+    }
+}
