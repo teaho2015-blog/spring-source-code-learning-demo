64070a7d056266dca8ed772a7970b31a42b18e69
teaho2015 <teaho2015@gamil.com>
2021-11-20 18:08:13 +0800

    [upd]

--- spring-boot-mvc-inside-out-demo/src/main/java/net/teaho/demo/spring/mvc/insideout/controller/DemoController.java
+++ spring-boot-mvc-inside-out-demo/src/main/java/net/teaho/demo/spring/mvc/insideout/controller/DemoController.java
@@ -1,10 +1,10 @@
 package net.teaho.demo.spring.mvc.insideout.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -17,6 +17,25 @@ public class DemoController {
     @PostMapping("/demo/time")
     public Map<String, String> demo() {
         return Collections.singletonMap("time", LocalDateTime.now().toString());
+    }
+
+    @PostMapping("/demo2/time")
+    public Map<String, String> demo2(@RequestParam String key) {
+        return Collections.singletonMap("time", key + LocalDateTime.now());
+    }
+
+    @PostMapping("/demo3/time")
+    public Map<String, String> demo3(@RequestParam String key) {
+        throw new IllegalStateException("demo3 exception!!!");
+    }
+
+    @PostMapping("/api/body1")
+    public Map<String, Object> body1(@RequestParam String id, Model model) {
+
+        Map<String, Object> map = new HashMap<>();
+        map.put("id", id);
+        map.put("model", model);
+        return map;
     }
 
 
---
+++ spring-boot-mvc-inside-out-demo/src/main/java/net/teaho/demo/spring/mvc/insideout/controller/Model.java
@@ -0,0 +1,15 @@
+package net.teaho.demo.spring.mvc.insideout.controller;
+
+import lombok.Data;
+
+/**
+ * @author hetingleong@xiaomi.com
+ * @date 2021-11
+ */
+@Data
+public class Model {
+
+    private Integer id;
+    private String content;
+
+}
