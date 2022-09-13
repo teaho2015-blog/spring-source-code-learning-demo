f3b7951e606f0a28404292e639dd9de57d15f589
teaho2015 <teaho2015@gamil.com>
2022-09-13 11:51:15 +0800

    [upd]example.

--- spring-cloud-zookeeper-config-demo/src/main/java/net/teaho/demo/springcloud/zookeeper/Example.java
+++ spring-cloud-zookeeper-config-demo/src/main/java/net/teaho/demo/springcloud/zookeeper/Example.java
@@ -1,13 +1,21 @@
 package net.teaho.demo.springcloud.zookeeper;
 
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.Configuration;
 
+import javax.annotation.PreDestroy;
+
 @RefreshScope
 @Configuration
 @ConfigurationProperties("global.sample")
-public class Example {
+public class Example implements InitializingBean {
+
+    public Example() {
+        System.out.println("Example construct.");
+    }
+
     /**
      * /config/global/spring-cloud-zk-config-demo,dev/global.sample.serverEnabled
      */
@@ -29,5 +37,15 @@ public class Example {
 
     public void setFeatureLogEnabled(String featureLogEnabled) {
         this.featureLogEnabled = featureLogEnabled;
+    }
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        System.out.println("Example afterPropertiesSet");
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Example destroy!");
     }
 }
