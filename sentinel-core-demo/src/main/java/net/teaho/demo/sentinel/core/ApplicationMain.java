e303881bc06f99dfe73e4a3b51ff1bf91c957291
teaho2015 <teaho2015@gamil.com>
2023-06-15 01:26:11 +0800

    [upd]sentinel core metric extension.

--- sentinel-core-demo/src/main/java/net/teaho/demo/sentinel/core/ApplicationMain.java
+++ sentinel-core-demo/src/main/java/net/teaho/demo/sentinel/core/ApplicationMain.java
@@ -1,7 +1,9 @@
 package net.teaho.demo.sentinel.core;
 
 import com.alibaba.csp.sentinel.Entry;
+import com.alibaba.csp.sentinel.EntryType;
 import com.alibaba.csp.sentinel.SphU;
+import com.alibaba.csp.sentinel.Tracer;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.csp.sentinel.slots.block.RuleConstant;
 import com.alibaba.csp.sentinel.slots.block.flow.FlowRule;
@@ -15,9 +17,10 @@ import java.util.concurrent.TimeUnit;
  * @author teaho2015@gmail.com
  * @date 2023-03
  */
-public class TestMain {
+public class ApplicationMain {
 
     public static void main(String[] args) {
+
         // 配置规则.
         initFlowRules();
 
@@ -25,13 +28,19 @@ public class TestMain {
             // 1.5.0 版本开始可以直接利用 try-with-resources 特性
             Entry entry = null;
             try {
-                SphU.entry("HelloWorld");
+                entry = SphU.entry("HelloWorld", EntryType.OUT, 1, new Object[0]);
                 // 被保护的逻辑
                 System.out.println("hello world");
                 TimeUnit.MILLISECONDS.sleep(100L);
+                if (System.currentTimeMillis() % 3 == 0) {
+                    throw new IllegalStateException();
+                }
             } catch (BlockException | InterruptedException ex) {
                 // 处理被流控的逻辑
                 System.out.println("blocked!");
+            } catch (Exception e) {
+                //DO NOTHING
+                Tracer.trace(e);
             } finally {
                 if (entry != null) {
                     entry.exit();
@@ -46,7 +55,7 @@ public class TestMain {
         rule.setResource("HelloWorld");
         rule.setGrade(RuleConstant.FLOW_GRADE_QPS);
         // Set limit QPS to 20.
-        rule.setCount(20);
+        rule.setCount(9);
         rules.add(rule);
         FlowRuleManager.loadRules(rules);
     }
---
+++ sentinel-core-demo/src/main/java/net/teaho/demo/sentinel/core/SimpleAdvancedMetricExtension.java
@@ -0,0 +1,70 @@
+package net.teaho.demo.sentinel.core;
+
+import com.alibaba.csp.sentinel.metric.extension.AdvancedMetricExtension;
+import com.alibaba.csp.sentinel.slotchain.ResourceWrapper;
+import com.alibaba.csp.sentinel.slots.block.BlockException;
+
+/**
+ * @author hetingleong@xiaomi.com
+ * @date 2023-06
+ */
+public class SimpleAdvancedMetricExtension implements AdvancedMetricExtension {
+
+    @Override
+    public void onPass(ResourceWrapper rw, int batchCount, Object[] args) {
+        System.out.println("onpass" + rw + batchCount + args);
+    }
+
+    @Override
+    public void onBlocked(ResourceWrapper rw, int batchCount, String origin, BlockException e, Object[] args) {
+        System.out.println("onBlocked" + rw + batchCount + origin + e + args);
+
+    }
+
+    @Override
+    public void onComplete(ResourceWrapper rw, long rt, int batchCount, Object[] args) {
+        System.out.println("onBlockonCompleteed" + rw + batchCount + rt + args);
+
+    }
+
+    @Override
+    public void onError(ResourceWrapper rw, Throwable throwable, int batchCount, Object[] args) {
+        System.out.println("onError" + rw + batchCount + throwable + args);
+
+    }
+
+    @Override
+    public void addPass(String resource, int n, Object... args) {
+
+    }
+
+    @Override
+    public void addBlock(String resource, int n, String origin, BlockException blockException, Object... args) {
+
+    }
+
+    @Override
+    public void addSuccess(String resource, int n, Object... args) {
+
+    }
+
+    @Override
+    public void addException(String resource, int n, Throwable throwable) {
+
+    }
+
+    @Override
+    public void addRt(String resource, long rt, Object... args) {
+
+    }
+
+    @Override
+    public void increaseThreadNum(String resource, Object... args) {
+
+    }
+
+    @Override
+    public void decreaseThreadNum(String resource, Object... args) {
+
+    }
+}
